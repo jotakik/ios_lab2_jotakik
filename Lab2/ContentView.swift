74504958a87daaec3cebd608866a8f64c789ca07
Joshua Tan <jxt208@nyu.edu>
2025-10-09 00:18:56 -0700

    Initial Commit

---
+++ Lab2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab2
+//
+//  Created by Joshua Tan on 2025/10/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Joshua Tan on 2025/10/9.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
